027b35efcfca8123240bb7ba1711ce789d0ed736
Tishaaa <tisha.startern@gmail.com>
2019-06-23 18:18:54 +0600

    get location list

--- DemoProject/src/basics.java
+++ DemoProject/src/basics.java
@@ -15,31 +15,18 @@ public class basics {
 	 * @param args
 	 */
 	@Test()
-    public void test1() {
+    public void getLocation() {
 		// TODO Auto-generated method stub
-		String remembertoken = "3UeQw0wEvDjekl9gWRcigCLqWWJrds5SpqrbDlqwVsYWzSB1HQTDAxNJaz4GplyzRe46FkhPXXMcUOtZRllUw6ACnPuwYWFN1CSIai7HDgnLv3tX6vfE6YnBtOfJbxntXrVm7sZ5I9WxQa7RZRW14l3J69kv3pJjqThzuHKNiLzYqhoBafS6HNB7nbuvcgNsIn3ocmo2IGCngcFSxTACD2PjWENxJ0uSlcYxElT9qKOOx9cIgw28P9PxPzbSloX";
-		String lat_data = "23.7897222";
-		String lng_data = "90.41962439999998";
+		
 		RestAssured.baseURI ="http://api.dev-sheba.xyz"; 
-		http://api.dev-sheba.xyz/v2/affiliates/34/lite-sp-pending
-		given().
-		       param("remember_token",remembertoken).
-		       param("lat",lat_data).
-		       param("lng",lng_data).
-		       when().
-		       get(generateResource("34")).
-		       then().assertThat().statusCode(200).and().contentType(ContentType.JSON).and().
-		       body("partners[0].moderation_status", equalTo("pending")).and().
-		       body("partners[0].location", equalTo("Gulshan"));
+		given().header("app-key", "11111", "app-secret","c1Dutiw4yP").
+		when().get("/v1/vendors/locations").
+		then().statusCode(200);
 		       
 
 	}
 	
 	
-	public static String generateResource(String id)
-	{
-		return "/v2/affiliates/"+id+"/lite-sp-pending";
-	}
 	
 	
 	/*public static int getRandomAffiliateId() {
--- DemoProject/src/locationList.java
+++ DemoProject/src/locationList.java
@@ -1,4 +1,36 @@
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.response.ResponseBodyData;
 
 public class locationList {
+	public static ResponseBody response;
+    public static String jsonAsString;
+	@Test
+	public void test1() {
+		// TODO Auto-generated method stub
+		RestAssured.baseURI ="http://api.dev-sheba.xyz"; 
+		response = 
+		given()
+		.header("app-key","111111","app-secret","c1Dutiw4yP")
+		.when()
+		.get("/v1/vendors/locations")
+		.then().assertThat().statusCode(200)
+		.contentType(ContentType.JSON)
+		.extract().response();
+		 jsonAsString = response.asString();
+		 //System.out.println(jsonAsString);
+		
+		       
 
+	}
+	
+	
+	
 }
