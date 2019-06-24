a4b062885e8dd011a1c9eb8faa17d4e7e0d74d6d
Tishaaa <tisha.startern@gmail.com>
2019-06-24 21:04:26 +0600

    grab response

--- DemoProject/src/locationList.java
+++ DemoProject/src/locationList.java
@@ -1,31 +1,40 @@
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
+
+import java.util.List;
+
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.parsing.Parser;
 import io.restassured.response.Response;
 import io.restassured.response.ResponseBody;
 import io.restassured.response.ResponseBodyData;
 
 public class locationList {
-	public static ResponseBody response;
+	//public static ResponseBody response;
     public static String jsonAsString;
 	@Test
 	public void test1() {
 		// TODO Auto-generated method stub
 		RestAssured.baseURI ="http://api.dev-sheba.xyz"; 
-		response = 
+		RestAssured.defaultParser = Parser.JSON;
+		Response response = 
 		given()
-		.header("app-key","111111","app-secret","c1Dutiw4yP")
+		.header("app-key","111111")
+		.header("app-secret","c1Dutiw4yP")
 		.when()
 		.get("/v1/vendors/locations")
 		.then().assertThat().statusCode(200)
 		.contentType(ContentType.JSON)
 		.extract().response();
-		 jsonAsString = response.asString();
-		 //System.out.println(jsonAsString);
+		List <String> jsonResponse = response.jsonPath().getList("$");
+		//System.out.println(jsonResponse.size());
+		//jsonAsString = response.asString();
+		//System.out.println(jsonAsString);
+		
 		
 		       
 
